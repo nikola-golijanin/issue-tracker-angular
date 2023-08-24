cf9975e3315bb05fde1fdbba5b401693eae9fb8e
nikola-golijanin <golijanin.nikola@outlook.com>
2023-08-24 21:49:33 +0200

    Resolving issues

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -10,11 +10,21 @@ import { Issue } from '../issue';
 export class IssueListComponent implements OnInit {
   issues: Issue[] = [];
   showReportIssue = false;
+  selectedIssue: Issue | null = null;
+
 
   constructor(private issueService: IssuesService) {}
 
   ngOnInit(): void {
     this.getIssues();
+  }
+
+  onConfirm(confirmed: boolean) {
+    if (confirmed && this.selectedIssue) {
+      this.issueService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
   }
 
   private getIssues() {
